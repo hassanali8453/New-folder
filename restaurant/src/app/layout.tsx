51c36bc341052242ebc6acbbf4ae265aabf5bbef
hassanali8453 <hassanali37389@gmail.com>
2024-11-21 12:24:07 +0500

    made some other pages

--- restaurant/src/app/layout.tsx
+++ restaurant/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
 import NavBar from "@/Components/View/Header/Navbar";
 import Footer from "@/Components/View/Footer";
 
